ed0b2e7e99d6371f423aa0e8337a1b01106422eb
Luciano Tejeda <luc.tejeda@duocuc.cl>
2024-11-03 23:48:29 -0300

    Agrega la funcion de Camara

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,21 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+
   username: string = '';
   isLoggedIn: boolean = false;
+  userIcon: string = 'https://w7.pngwing.com/pngs/981/645/png-transparent-default-profile-united-states-computer-icons-desktop-free-high-quality-person-icon-miscellaneous-silhouette-symbol.png';
 
   constructor(private router: Router) {}
 
   ngOnInit() {
     const storedUsername = localStorage.getItem('username');
+    const storedIcon = localStorage.getItem('userIcon');
+
     if (storedUsername) {
       this.username = storedUsername;
       this.isLoggedIn = true;
+    }
+    if (storedIcon) {
+      this.userIcon = storedIcon;
     }
   }
 
@@ -23,14 +32,37 @@ export class LoginPage implements OnInit {
     if (this.username) {
       this.isLoggedIn = true;
       localStorage.setItem('username', this.username);
-      localStorage.removeItem('chatMessages'); // Limpiar los mensajes del chat
+      localStorage.removeItem('chatMessages');
       this.router.navigate(['/tabs/tab1'], { queryParams: { username: this.username } });
     }
   }
 
+  
+
   logout() {
     this.isLoggedIn = false;
     this.username = '';
+    this.userIcon = 'assets/default-icon.png';
     localStorage.removeItem('username');
+    localStorage.removeItem('userIcon');
+  }
+
+  async changeIcon() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Prompt,
+      });
+
+      this.userIcon = image.webPath || this.userIcon;
+
+      if (image.webPath) {
+        localStorage.setItem('userIcon', image.webPath);
+      }
+    } catch (error) {
+      console.error('Error al cambiar el icono:', error);
+    }
   }
 }
